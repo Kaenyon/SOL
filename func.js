1b8a160cfce26109de14ba571ae1cb5324d06645
sailor02 <sailor0289@gmail.com>
2018-02-13 02:19:06 +0900

    html highligther

--- func.js
+++ func.js
@@ -9,12 +9,8 @@ function searchKeyPress(e){
 function search_problem(num) {
   if(num < 1000 || num > 15500) alert("올바른 문제 번호를 입력해주세요");
   else{
-    let name = "s"+num;
-    if(name in window){
-      document.getElementById("solution").style.display = "block";
-      document.getElementById("sol_num").innerHTML = "Solution# " + num;
-      document.getElementById("source").innerHTML = window[name];
-    }
-    else alert("풀이가 아직 업로드되지 않았습니다");
+    let url = "https://sailor02.github.io/SOL/" + num;
+    window.location(url);
+    /*else alert("풀이가 아직 업로드되지 않았습니다");*/
   }
 }
