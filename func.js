458b64d3627ce2b3c8f026cae3720c6492d8ce85
sailor02 <sailor0289@gmail.com>
2018-02-13 10:57:03 +0900

    html func00

--- func.js
+++ func.js
@@ -1,12 +1,12 @@
-function searchKeyPress(e){
+var searchKeyPress = function (e){
   e = e || window.event;
   if (e.keyCode == 13){
     document.getElementById('btn').click();
     return false;
   }
   return true;
-}
-function search_problem(num) {
+};
+var search_problem function (num) {
   if(num < 1000 || num > 15500) alert("올바른 문제 번호를 입력해주세요");
   else{
     alert(num);
@@ -14,4 +14,4 @@ function search_problem(num) {
     window.location(url);
     /*else alert("풀이가 아직 업로드되지 않았습니다");*/
   }
-}
+};
